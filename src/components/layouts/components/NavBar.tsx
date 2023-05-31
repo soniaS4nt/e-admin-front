3245b7be09652da3032b759790cd2283f68acf4b
“sonia” <soniachaparro21@gmail.com>
2023-05-30 22:49:57 -0400

    Add roote to home

--- src/components/layouts/components/NavBar.tsx
+++ src/components/layouts/components/NavBar.tsx
@@ -2,16 +2,17 @@ import LogoIcon from "@/components/icons/LogoIcon";
 import SearchIcon from "@/components/icons/SearchIcons";
 import UserIcon from "@/components/icons/UserIcon";
 import { TextInput } from "@tremor/react";
+import Link from "next/link";
 
 export default function NavBar() {
   return (
     <nav className="w-full flex flex-row justify-between">
-      <div className=" flex flex-row items-center p-5">
+      <Link href="/" className=" flex flex-row items-center p-5">
         <LogoIcon width={"60px"} height={"60px"} />
         <h1 className="font-bold text-lg">
           E-admin <span className="text-orange-400 font-extrabold">.</span>
         </h1>
-      </div>
+      </Link>
 
       <div className="flex flex-row items-center p-5 mx-5">
         <TextInput
