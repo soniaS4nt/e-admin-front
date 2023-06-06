6f3285a486fda0e6f3bb341c2e7294d1c1889fd0
“sonia” <soniachaparro21@gmail.com>
2023-06-06 16:53:11 -0400

    add selectItem

---
+++ src/helpers/selectItems.ts
@@ -0,0 +1,6 @@
+import { SelectItems } from "@/interfaces/select.interface";
+
+export const selectItems: SelectItems[] = [
+  { id: 0, value: "Mujer", text: "Mujer" },
+  { id: 1, value: "Hombre", text: "Hombre" },
+];
---
+++ src/interfaces/select.interface.ts
@@ -0,0 +1,5 @@
+export interface SelectItems {
+  id: number;
+  value: string;
+  text: string;
+}
